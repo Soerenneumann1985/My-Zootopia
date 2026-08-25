50ca94436e0069b41aa3fdf65f342280296b80e1
Sören Neumann <soerenneumann1985@gmail.com>
2026-08-25 14:26:04 +0200

    Tierdaten in HTML eingefügt

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,27 +5,49 @@ def load_data(file_path):
     with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
-def print_animals(data):
-    """ Print selected fields for each animal """
-    for animal in data:
-        print("-----")
+def load_templates(template_path):
+    """ Load the HTML templates """
+    with open(template_path, "r", encoding="utf-8") as handle:
+        return handle.read()
 
+
+def build_animals_string(data):
+    """ Build an animals string """
+    output = ""
+
+    for animal in data:
         # Name
         if "name" in animal:
-            print("Name:", animal["name"])
+            output += f"Name: {animal['name']}\n"
 
         # Ernährung (diet)
         if "characteristics" in animal and "diet" in animal["characteristics"]:
-            print("Ernährung:", animal["characteristics"]["diet"])
+            output += f"Ernährung: {animal['characteristics']['diet']}\n"
 
         # Erster Ort
         if "locations" in animal and len(animal["locations"]) > 0:
-            print("Ort:", animal["locations"][0])
+            output += f"Ort: {animal['locations'][0]}\n"
 
         # Typ
         if "characteristics" in animal and "type" in animal["characteristics"]:
-            print("Typ:", animal["characteristics"]["type"])
+            output += f"Typ: {animal['characteristics']['type']}\n"
 
-# Programm starten
+    return output
+
+def write_output(html_content, output_path):
+    """ Write an HTML output """
+    with open(output_path, "w", encoding="utf-8") as handle:
+        handle.write(html_content)
+
+# ---------------------
+# Hauptprogramm
+# ---------------------
+
 animals = load_data("animals_data.json")
-print_animals(animals)
+template = load_templates("animals_template.html")
+
+animals_string = build_animals_string(animals)
+
+final_html = template.replace("__REPLACE_ANIMALS_INFO__", animals_string)
+
+write_output(final_html, "animals.html")
